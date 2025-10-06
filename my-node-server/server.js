6ab59c1b0765fdf0a8d763f85d5fd773dc7b93a1
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-10-06 21:56:51 +0700

    aaa

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 5000;
-const cors = require('cors');
 app.use(cors());
 
 app.get('/', (req, res) => {
